3d99306e4ade3abebe1ce26590b2faf6c9459193
Cristhian Fernandez <crisfervil@users.noreply.github.com>
2020-03-29 15:45:53 +0100

    First Commit

---
+++ src/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Microsoft.Powerplatform.Cds.Client;
+using Microsoft.Xrm.Sdk;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string cdsConnectionString, string solutionName, FileInfo outputFile)
+        {
+
+            if(string.IsNullOrEmpty(cdsConnectionString)) throw new ArgumentException("cdsConnectionString can't be empty");
+            var cdsClient = new CdsServiceClient(cdsConnectionString);
+
+            if(cdsClient.IsReady){
+                // var whoAmIRequest =  ();
+                // cdsClient.Execute(whoAmIRequest);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
